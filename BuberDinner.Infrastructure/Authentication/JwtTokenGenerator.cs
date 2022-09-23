76da7db3f3cad9e75625ab8233ca3062fabba21b
Wenjun Xie <xwj.june@gmail.com>
2022-09-23 15:40:02 +1200

    init jwt token generator

---
+++ BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,42 @@
+using System.Linq.Expressions;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using BuberDinner.Application.Common.Interfaces.Services;
+
+namespace BuberDinner.Infrastructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public string GenerateToken(Guid userId, string firstName, string lastName)
+    {
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super-secret-key")),
+            SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: "BuberDinner",
+            expires: _dateTimeProvider.UtcNow.AddMinutes(60),
+            claims: claims,
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+}
--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,3 +1,7 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
+using BuberDinner.Infrastructure.Authentication;
+using BuberDinner.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Infrastructure;
@@ -6,6 +10,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
